204e79bad124bd1bea71294feb0a8f5d66bac652
HelmuthMN <helmuthmullernetto@gmail.com>
2021-05-31 21:44:53 -0300

    create date filter

--- app/dataConsumer.py
+++ app/dataConsumer.py
@@ -16,15 +16,16 @@ def consumingData():
 
 
 #Check if the date on url is the same of the API
-def jsonToDictPerData(date):
+def consumingDataPerDate(date):
     #import ipdb; ipdb.set_trace()
     dataRequest = requests.get(f'{baseurl}/daily/{date}')
     dataJson = dataRequest.json()
-    return dataJson
+    return filterBrazil(dataJson)
 
 
 #Store data in [blank] dictionary 
 def jsonToDict(dataJson):
+
     dictStates = {}
 
     for row in dataJson:
@@ -37,4 +38,21 @@ def jsonToDict(dataJson):
             'casos ativos' : row['active']
         }
     #import ipdb; ipdb.set_trace()
+    return dictStates
+
+
+def filterBrazil(dataJson):
+    dictStates = {}
+
+    for row in dataJson:
+        if row['countryRegion'] == 'Brazil':
+            state = row['provinceState']
+            dictStates[ini.initialStates[state]] = {
+                'estado' : row['provinceState'],
+                'confirmados' : row['confirmed'],
+                'recuperados' : row['recovered'],
+                'mortos' : row['deaths'],
+                'casos ativos' : row['active']
+            }
+            #import ipdb; ipdb.set_trace()
     return dictStates
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from app.dataConsumer import consumingData, jsonToDictPerData
+from app.dataConsumer import consumingData, consumingDataPerDate as dpd
 from datetime import date,timedelta
 
 defaultDay = date.today()-timedelta(days=1)
@@ -14,20 +14,20 @@ def index():
 
 @app.route('/general')
 def showCountry():
-    test = consumingData()
-    return str(test)
+    data = consumingData()
+    return str(data)
 
 
 @app.route('/state/<string:state>')
 def showState(state):
     state = state.upper()
-    test = consumingData()
-    return str(test[state])
+    data = consumingData()
+    return str(data[state])
 
 
 @app.route('/date/')
 @app.route('/date/<string:day>')
 def showDate(day=defaultDay):
-    test = jsonToDictPerData(day)
+    data = dpd(day)
     #import ipdb; ipdb.set_trace()
-    return str(test)
+    return str(data)
