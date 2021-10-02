75a94e584080c46545761b4f1aa281ff6f4f1983
Luan Torres <luantorres360@gmail.com>
2021-10-02 18:59:32 -0300

    Fixing Google News API

--- app/news.py
+++ app/news.py
@@ -8,19 +8,41 @@ def callGoogle(state):
     try:
         googlenews = GoogleNews(lang='pt')
         googlenews.search('covid ' + state)
-        newsData = googlenews.get_texts()
+        newsData = googlenews.results(sort=True)
+        returned_dict = {}
 
         if state == 'Brasil':
-            return newsData[0]
+            returned_dict['titulo'] = newsData[0]['title']
+            returned_dict['desc'] = newsData[0]['desc']
+            returned_dict['link'] = newsData[0]['link']
+            returned_dict['fonte'] = newsData[0]['media']
+            returned_dict['data'] = newsData[0]['date']
+            return returned_dict
 
         for row in newsData:
-            this_row = unidecode(row)
+            this_row = row['title']
             if state in this_row \
                 or initialStates[state] in this_row \
                 or state.split(' ')[0] in this_row:
-                return row
+                returned_dict['titulo'] = row['title']
+                returned_dict['desc'] = row['desc']
+                returned_dict['link'] = row['link']
+                returned_dict['fonte'] = row['media']
+                returned_dict['data'] = row['date']
+                return returned_dict
             elif state == 'Minas Gerais' and ('BH' or 'bh') in this_row:
-                return row
-        return newsData[0]
+                returned_dict['titulo'] = row['title']
+                returned_dict['desc'] = row['desc']
+                returned_dict['link'] = row['link']
+                returned_dict['fonte'] = row['media']
+                returned_dict['data'] = row['date']
+                return returned_dict
+        
+        returned_dict['titulo'] = newsData[0]['title']
+        returned_dict['desc'] = newsData[0]['desc']
+        returned_dict['link'] = newsData[0]['link']
+        returned_dict['fonte'] = newsData[0]['media']
+        returned_dict['data'] = newsData[0]['date']
+        return returned_dict
     except:
         return 'Google News API is not working'
